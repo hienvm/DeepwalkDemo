53c680bed73d968014b6df486403662855c2f294
hienvm <hien2842003@gmail.com>
2024-05-26 01:31:33 +0700

    update parallelization + TSNE

--- utils/deepwalk.py
+++ utils/deepwalk.py
@@ -1,15 +1,17 @@
 import random
 import torch
 import torch.nn as nn
-from torch import tensor
+from torch import Tensor, tensor
+import torch.optim as optim
+import torch.multiprocessing as mp
+import tqdm
 
 from utils.similarity import similarity
-from utils.skipgram import skip_gram
 from utils.walks import random_walk
 
 
 class DeepwalkModel(nn.Module):
-    def __init__(self, V, emb_sz, leaf_pos) -> None:
+    def __init__(self, V: int, emb_sz: int, leaf_pos: list) -> None:
         super(DeepwalkModel, self).__init__()
         # Hidden layer biểu diễn vector cho đỉnh
         self.embedding_layer = nn.Embedding(
@@ -28,7 +30,7 @@ class DeepwalkModel(nn.Module):
         # thứ tự lá
         self.leaf_pos = leaf_pos
 
-    def forward(self, u, v):
+    def forward(self, u: int, v: int):
         """Đoán Pr(u | v_emb), trả về loss function"""
         loss = tensor(0.0)
 
@@ -62,23 +64,86 @@ class DeepwalkModel(nn.Module):
         return loss
 
 
-def train_deepwalk(model, optimizer, vertices, adj_lists, loss_records, scheduler, walk_len, walks_per_vertex, window_sz):
-    for i in range(walks_per_vertex):
+def train_deepwalk(
+    model: nn.Module,
+    start_lr: float,
+    end_lr: float,
+    vertices: list,
+    adj_lists: list[list],
+    loss_records: list,
+    walk_len: int,
+    walks_per_vertex: int,
+    window_sz: int,
+    worker_threads: int,
+    chunk_sz: int,
+):
+    lr_step = (start_lr - end_lr) / walks_per_vertex
+    for i in tqdm.tqdm(range(walks_per_vertex)):
         epoch_loss = 0.0
         cnt = 0
         random.shuffle(vertices)
-        for src in vertices:
-            # lấy ra đường đi ngẫu nhiên
-            walk = random_walk(src, adj_lists, walk_len)
-            # huấn luyện skipgram
-            (running_loss, running_cnt) = skip_gram(
-                walk=walk, max_window_size=window_sz, optimizer=optimizer, model=model)
-            # Ghi nhận lại hàm mất mát
-            epoch_loss += running_loss
-            cnt += running_cnt
+        if worker_threads > 1:
+            with mp.Pool() as pool:
+                # sinh ra corpus và chia thành một batch cho mỗi walk từ một đỉnh nguồn
+                batches = [
+                    (
+                        random_walk(v, adj_lists, walk_len),
+                        window_sz,
+                        start_lr,
+                        model,
+                    ) for v in vertices
+                ]
+                # huấn luyện skipgram
+                results = pool.starmap(
+                    func=skip_gram_deepwalk,
+                    iterable=batches,
+                    chunksize=chunk_sz,
+                )
+            for (running_loss, running_cnt) in results:
+                # Ghi nhận lại hàm mất mát
+                epoch_loss += running_loss
+                cnt += running_cnt
+        else:
+            for src in vertices:
+                # lấy ra đường đi ngẫu nhiên
+                walk = random_walk(src, adj_lists, walk_len)
+                # huấn luyện skipgram
+                (running_loss, running_cnt) = skip_gram_deepwalk(
+                    walk=walk,
+                    window_sz=window_sz,
+                    lr=start_lr,
+                    model=model
+                )
+                # Ghi nhận lại hàm mất mát
+                epoch_loss += running_loss
+                cnt += running_cnt
 
         # tính trung bình hàm mất mát cho lần chạy
         loss_records.append(epoch_loss / cnt)
 
         # Giảm tuyến tính tỉ lệ học
-        scheduler.step()
+        start_lr -= lr_step
+
+
+def skip_gram_deepwalk(walk: list, window_sz: int, lr: float, model: nn.Module):
+    optimizer = optim.SGD(model.parameters(), lr=lr)
+    running_loss = 0.0
+    cnt = 0
+    # soft window
+    w = random.randint(1, window_sz)
+    for j, v in enumerate(walk):
+        window = walk[j - w: j] + walk[j + 1: j + w + 1]
+        for u in window:
+            # reset gradient về 0
+            optimizer.zero_grad()
+            # tính hàm mất mát
+            loss: Tensor = model(u, v)
+            # lan truyền ngược để tính gradient cho các parameter
+            loss.backward()
+            # tối ưu các parameter với gradient tính đc
+            optimizer.step()
+
+            # Ghi nhận lại hàm mất mát
+            running_loss += loss.item()
+            cnt += 1
+    return (running_loss, cnt)
--- utils/similarity.py
+++ utils/similarity.py
@@ -2,10 +2,7 @@ from torch import tensor, Tensor
 
 
 def similarity(u: Tensor, v: Tensor) -> Tensor:
-    # Dùng khoảng cách euclid cho 1D
-    if u.dim() == 0:
-        return u.subtract(v).abs()
+    # có thể dùng khoảng cách euclid cho 1D
+    # if u.dim() == 0:
+    #     return u.subtract(v).abs()
     return u.dot(v)
-
-
-print(similarity(tensor([1, 2]), tensor([2, 3])))
